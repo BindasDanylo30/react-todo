8f735afb55965539dfad185450a766559c0eb716
Daniel <danilbindas@gmail.com>
2025-10-20 19:42:08 +0200

    get by components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import classes from "./Heade.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <a href="/" className={classes.link}>
+          ToDo
+        </a>
+      </div>
+    </header>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assetc/scss/normalize.scss";
 import "./assetc/scss/style.scss";
-import { ToDoList } from "./pages/ToDoList";
+import { ToDoListPage } from "./pages/ToDoListPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <ToDoListPage />
   </React.StrictMode>
 );
 
